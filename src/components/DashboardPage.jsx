8b433aafc14d764bfe5c376e7f750759bbe5192b
ribpy <mdriazbappy22@gmail.com>
2024-12-09 23:59:38 +0600

    simple update

--- src/components/DashboardPage.jsx
+++ src/components/DashboardPage.jsx
@@ -9,14 +9,17 @@ import UserNav from "./UserNav";
 const DashboardPage = () => (
   <div className="flex h-max flex-col border border-gray-300 m-3 shadow-md">
     <div className="border-b">
-      <div className="flex items-center space-x-4 p-5">
-        <span className="font-semibold">Dashboard</span>/
+      <div className="flex items-center p-5">
+        <span className="font-semibold">Dashboard </span>
+        <span className="px-1">|</span>
         <span className="font-semibold">Metronome</span>
       </div>
       <div className="flex h-16 items-center px-5">
         <Cloud />
         <MainNav className="mx-6" />
-        <div className="ml-auto flex items-center space-x-4"><UserNav/></div>
+        <div className="ml-auto flex items-center space-x-4">
+          <UserNav />
+        </div>
       </div>
     </div>
     <div className="flex-1 space-y-4 p-8 pt-6">
